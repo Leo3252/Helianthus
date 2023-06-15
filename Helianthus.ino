f08e1dccb1cfe79acc88349ced31043e9858759d
Leo3252 <leo_huang22@yahoo.com>
2023-06-15 12:34:23 +0200

    Added comments to FINAL

--- Helianthus.ino
+++ Helianthus.ino
@@ -3,33 +3,36 @@
 Servo xServo; 
 Servo yServo;
 
+//Fixed values
 const int rightLdrPin = A0; 
 const int leftLdrPin = A1; 
 const int xPin = 9;
 const int yPin = 10;
 
+//Declaring variables
 int rightLdrValue = 0;
 int leftLdrValue = 0; 
 int anglePositionAtHighestValueX = 0; //variable to store highest light value
 int anglePositionAtHighestValueY = 0;
-int currentAngleX; //Used to alter the current x position
+int currentAngleX; //Used to alter the current angle positions
 int currentAngleY;
 int lightSourceToFollow;
-
-int counter = 0;
+int counter = 0; //To calc the number of rotations made by servo
 bool done = false; //flag
-const int lightSourceOffset = 200;
-const int ambientLightOffset = 400;
+
+//Alter-able value
+const int lightSourceOffset = 200; //To keep following the initial light intensity
+const int ambientLightOffset = 400; //To make sure you're pointing at a light source
 
 //OBTAINING WATTAGE:
 const int voltagePin = A2;
 const int currentPin = A3;
-const int numberToMultiply = 5;
+const int numberToMultiply = 5; //Convertion
 
 //Bluetooth
 char receivedData;
-String operation = "FOLLOW_LIGHT";
-bool infoPresent;
+String operation = "FOLLOW_LIGHT"; //Initial op. of the method
+bool infoPresent; //If there's info flag
 
 void setup() { //initiate everything
   xServo.attach(xPin);
@@ -40,11 +43,11 @@ void setup() { //initiate everything
 void loop() {
  
   if (!done) { //if no highest light source found keep searching
-   searchForLight('x', 180, false);
-   searchForLight('y', 95, true);
+   searchForLight('x', 180, false); //Search at x axis, through 180º, don't ask for highest light source
+   searchForLight('y', 95, true); //Search at y axis, through 95º, ask for highest light source value
    
-   if (!lightSourceFound) {
-     searchForLight('x', 180, false); //If there's still no light source found keep going
+   if (!lightSourceFound) { //If there's still no light source found keep going
+     searchForLight('x', 180, false); //Same as above
      searchForLight('y', 95, true);
    } 
    else if (lightSourceFound) { //When found
@@ -52,25 +55,79 @@ void loop() {
     }
   }
   
-  determineOperation();
+  determineOperation(); //We need to find out what task should we carry out from the options below
   
-  if(operation == "READ_WATTAGE") {
-    outputWattageToApp(); 
-    delay(900);
+  //Tasks
+  //We initialized operation to follow light so that it'll follow the light until otherwise
+  if(operation == "READ_WATTAGE") { //When operation is set to the wattage reader do:
+    outputWattageToApp(); //Sent wattage info to app
+    delay(900); //This plus the 100ms delay at the end sum to 1sec, which is the required interval
   } 
-  else if(operation == "MOVE_TRACKER") {
-    if(!infoPresent) {return;}
-    interpretData(receivedData);
+  else if(operation == "MOVE_TRACKER") { //When operation is set to manually move tracker do:
+    /*The code below is important: We set operation to move tracker but if no bool flag present
+    operation will remain as move tracker and it will continue to move the robot in the first direction 
+    you give. To fix this we need to know when instructions are coming in from the app to move the tracker
+    w/o compromising the current method (using variable "operation" to define task). 
+    So, a run down on how it works:
+    First call of void loop, operation is MOVE TRACKER, because no buttons are pressed on the app we "return"
+    to escape the else if statement.
+    Second call of void loop, we get button pressed, infoPresent becomes true, so we them interpret the data 
+    coming from the app and act accordingly.
+    */
+    if(!infoPresent) {return;} 
+    interpretData(receivedData); //Parameter is the data we receive
   } 
-  else if(operation == "FOLLOW_LIGHT") {
-    followLight();
+  else if(operation == "FOLLOW_LIGHT") { //When no operaion is running we:
+    followLight(); //Follow light
   }
   
+  //MUST IMPLEMENT INSIDE INTERPRET DATA SO THAT IF SUM IS ABOVE THRESHOLD DONT MOVE-----------------------------------------
   checkLogic();
 
   delay(100);
 }
-//METHODS..........................................
+//METHODS in order of appearance........................................................
+
+//Optimized so that it can process x and y directions without the need of two different methods
+void searchForLight(char axis, int maxAngleRange, bool needLightSource ) { //Last parameter is to prevent assigning highest light value when not required
+  
+  int currentHighestLightValue = 0; //Used to compare between the highest and current light values
+  
+  if(axis == 'x') {xServo.write(0);} //This is a tradeoff if I were to optimize the code
+  else if (axis == 'y') {yServo.write(0);} //Initializes servomotor to 0 degrees so that it can scan around
+  
+  for(int angle = 0; angle <= maxAngleRange; angle++) { //Using the value given by the parameter, the scan will go up until it satisifies the statement
+    
+    int ldrMeanValue; //New mean value every iteration
+    rightLdrValue = analogRead(rightLdrPin); //Update ldr values for each iteration
+    leftLdrValue = analogRead(leftLdrPin); 
+    
+    ldrMeanValue = (rightLdrValue + leftLdrValue) / 2;  //Calc mean
+    
+    if(ldrMeanValue > currentHighestLightValue) { //If value surpasses previous highest value do: 
+      currentHighestLightValue = ldrMeanValue; //Set new highest value
+      
+      if(axis == 'x') {anglePositionAtHighestValueX = angle;} //Stores the angle positions so that it can later, retrace its steps
+      else if (axis == 'y') {anglePositionAtHighestValueY = angle;}
+    }
+    
+    if(axis == 'x') {xServo.write(angle);} //Move the servo by 1 degree every iteration
+    else if (axis == 'y') {yServo.write(angle);}
+    
+    delay(50);
+  }
+
+  //Set servo to the highest light intensity location 
+  if(axis == 'x') {xServo.write(anglePositionAtHighestValueX); currentAngleX = anglePositionAtHighestValueX;}
+  else if (axis == 'y') {yServo.write(anglePositionAtHighestValueY); currentAngleY = anglePositionAtHighestValueY;}
+   
+  if (currentHighestLightValue > ambientLightOffset && needLightSource) { //If the highest light source is above a threshold do (So that it ignores ambient light)
+    lightSourceFound(1); //Manually set lightsourcefound to true
+    lightSourceToFollow = currentHighestLightValue; //This is a value that will be used later on by follow light to make sure it is following that value..........
+  } else {lightSourceFound(0);} //Manually set lightsourcefound to false
+}
+
+//CHECK LOGIC LATER-----------------------------------------------------------------------------------------
 bool lightSourceFound(int state) {
  
  if(state == 1) {
@@ -80,87 +137,28 @@ bool lightSourceFound(int state) {
   }
 }
 
-void searchForLight(char axis,int maxAngleRange, bool needLightSource ) { //Optimized so that it can process x and y directions..........Avoid last operation on second run
-  
-  int currentHighestLightValue = 0; //Used to compare between the highest and current light values
-  
-  if(axis == 'x') {xServo.write(0);} //This is a tradeoff if I were to optimize the code
-  else if (axis == 'y') {yServo.write(0);} //Initializes servomotor to 0 degrees so that it can scan around
-  for(int angle = 0; angle <= maxAngleRange; angle++) { //Using the value given by the parameter, the scan will go up until it satisifies the statement
-    int ldrMeanValue;
-    rightLdrValue = analogRead(rightLdrPin); //Update ldr values for each iteration
-    leftLdrValue = analogRead(leftLdrPin); 
+void determineOperation() { //Find out what we're doing
+  if(Serial.available() > 0) { //If there's info sent do:
+    infoPresent = true; //A flag which is required for later operation
+    receivedData = Serial.read(); //Obtain data
     
-    ldrMeanValue = (rightLdrValue + leftLdrValue) / 2; 
-    if(ldrMeanValue > currentHighestLightValue) { 
-      currentHighestLightValue = ldrMeanValue;
-      if(axis == 'x') {anglePositionAtHighestValueX = angle;} //Stores the angle positions so that it can later go back to it
-      else if (axis == 'y') {anglePositionAtHighestValueY = angle;}
+    if(receivedData == 'x') { //If data matches char 'x' do:
+      operation = "MOVE_TRACKER"; //Initiate bluetooth operation
     }
-    
-    if(axis == 'x') {xServo.write(angle);} //Move the servo by 1 degree every iteration
-    else if (axis == 'y') {yServo.write(angle);}
-    
-    delay(50);
-  }
-  if(axis == 'x') {xServo.write(anglePositionAtHighestValueX); currentAngleX = anglePositionAtHighestValueX;} //Stores the angle positions so that it can later go back to it
-  else if (axis == 'y') {yServo.write(anglePositionAtHighestValueY); currentAngleY = anglePositionAtHighestValueY;}
-   
-  if (currentHighestLightValue > ambientLightOffset && needLightSource) { //If the highest light source is above a threshold do (So that it ignores ambient light)
-    lightSourceFound(1); //Manually set lightsourcefound to true
-    lightSourceToFollow = currentHighestLightValue; //This is a value that will be used later on by follow light to make sure it is following that value..........
-  } else {lightSourceFound(0);} //Manually set lightsourcefound to false
+    else if (receivedData == 'y'){ //Initiate wattage reader
+      operation = "READ_WATTAGE";
+    }
+    else if (receivedData == 'z') { //When connection is terminated do:
+      operation = "FOLLOW_LIGHT"; //Start following light
+    }
+  } else{infoPresent = false;} //If no info sent set flag to false
 }
 
-void followLight() { //After finding out highest light source follow it
-  checkLogic();
-  rightLdrValue = analogRead(rightLdrPin); //Start reading sensors
-  leftLdrValue = analogRead(leftLdrPin) ;
-  int ldrMeanValue = (rightLdrValue + leftLdrValue) / 2; 
-  int difference = abs(rightLdrValue - leftLdrValue); //Find the difference in value of both ldrs
-  bool move; 
-  bool move2;
-  if (difference >= 0 && difference <= 5) { //If the difference is within a threshold then don't move //need to implement light value............. Don't move if meanldr value below +- 100 lightsourcetofollow
-   move = false;
-  } else {move = true;}
-  if(ldrMeanValue <= lightSourceToFollow + lightSourceOffset && ldrMeanValue >= lightSourceToFollow - lightSourceOffset) {
-    move2 = true;
-  } else {move2 = false;}
-  if (rightLdrValue < leftLdrValue && move && move2) { //If rightLdrValue is less than the second and it is beyond the threshold do:
-    counter++;
-    currentAngleX--; //Increments its angle by one
-    xServo.write(currentAngleX); //write it to motor  
-  }
-  else if (rightLdrValue > leftLdrValue && move && move2) { //Same here
-    counter++;
-    currentAngleX++;
-    xServo.write(currentAngleX);
-    
-  }
-  
-  //After some repetition ideally
-  if (counter == 30) {
-    if(currentAngleX < 60 || currentAngleX > 120) {
-      searchForLight('y', 95, false);
-    } else if (currentAngleX >= 60 && currentAngleX <= 120) {
-      searchForLight('y', 60, false); 
-    }
-    counter = 0;
-    
-  }
-}
-
-void checkLogic() {
-  if (currentAngleX > 180) {
-    currentAngleX = 180;
-  } else if (currentAngleX < 0) {
-    currentAngleX = 0;
-  }
-  if (currentAngleY > 95) {
-    currentAngleY = 95;
-  } else if (currentAngleX < 0) {
-    currentAngleX = 0;
-  }
+void outputWattageToApp() { //We output wattage values to the app
+  float wattage = readWattage(); //Find wattage
+  wattage = abs(wattage); //Make it positive
+  byte outputWattage = (map(wattage*100, 0, 100, 0, 25500)/100); //Convert wattage value to 0,255 (Max 1W)
+  Serial.write(outputWattage);
 }
 
 float readWattage() {
@@ -168,32 +166,11 @@ float readWattage() {
   float voltage = (voltageValue/1023.0) * numberToMultiply;
   float current = analogRead(currentPin) * (numberToMultiply / 1023.0);
   current = (current - (numberToMultiply / 2.0)) / 0.185;
-  return voltage * current;
+  return voltage * current; //Return wattage in (W)
 }
-void determineOperation() {
-  if(Serial.available() > 0) {
-    infoPresent = true;
-    receivedData = Serial.read();
-    
-    if(receivedData == 'x') {
-      operation = "MOVE_TRACKER";
-    }
-    else if (receivedData == 'y'){
-      operation = "READ_WATTAGE";
-    }
-    else if (receivedData == 'z') { //When connection is about to terminate
-      operation = "FOLLOW_LIGHT";
-    }
-  } else{infoPresent = false;}
-}
-void outputWattageToApp() {
-  float wattage = readWattage();
-  wattage = abs(wattage);
-  byte outputWattage = (map(wattage*100, 0, 100, 0, 25500)/100);
-  Serial.write(outputWattage);
-}
-void interpretData(char data) {
-  if(data == '1') {
+
+void interpretData(char data) { //Find out what data is being sent by app
+  if(data == '1') { 
     currentAngleY = currentAngleY - 5;
     yServo.write(currentAngleY);
   } else if (data == '2') {
@@ -205,5 +182,63 @@ void interpretData(char data) {
   } else if (data == '4') {
     currentAngleX = currentAngleX - 10;
     xServo.write(currentAngleX);
+  }
+}
+
+void followLight() { //After finding out highest light source follow it 
+  checkLogic();
+  
+  rightLdrValue = analogRead(rightLdrPin); //Start reading sensors
+  leftLdrValue = analogRead(leftLdrPin) ;
+  
+  int ldrMeanValue = (rightLdrValue + leftLdrValue) / 2; 
+  int difference = abs(rightLdrValue - leftLdrValue); //Find the difference in value of both ldrs
+  
+  bool move; //We need to check to things
+  bool move2;
+  
+  if (difference >= 0 && difference <= 5) { //If the difference is within a threshold then don't move
+   move = false;
+  } else {move = true;}
+  
+  //Make sure we're following the same light source, unaffected by other's
+  if(ldrMeanValue <= lightSourceToFollow + lightSourceOffset && ldrMeanValue >= lightSourceToFollow - lightSourceOffset) { 
+    move2 = true;
+  } else {move2 = false;}
+  
+  if (rightLdrValue < leftLdrValue && move && move2) { //If rightLdrValue is less than the second and it is beyond the threshold do:
+    counter++; //Count so that we know when to update y-angle position
+    currentAngleX--; //Increments its angle by one
+    xServo.write(currentAngleX); //write it to motor  
+  }
+  else if (rightLdrValue > leftLdrValue && move && move2) { //Same here
+    counter++;
+    currentAngleX++;
+    xServo.write(currentAngleX);
+    
+  }
+  
+  //After 30 +- rotations check for y anlge
+  if (counter == 30) {
+    if(currentAngleX < 60 || currentAngleX > 120) {
+      searchForLight('y', 95, false);
+    } else if (currentAngleX >= 75 && currentAngleX <= 120) { //We are following the sun so it is higher-up at midday 
+      searchForLight('y', 60, false); 
+    }
+    counter = 0; //Reset the counter
+    
+  }
+}
+
+void checkLogic() { //If current angle is above or below allowed threshold then set them back to original value
+  if (currentAngleX > 180) {
+    currentAngleX = 180;
+  } else if (currentAngleX < 0) {
+    currentAngleX = 0;
+  }
+  if (currentAngleY > 95) {
+    currentAngleY = 95;
+  } else if (currentAngleX < 0) {
+    currentAngleX = 0;
   }
 }
